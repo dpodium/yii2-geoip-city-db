4af97969a1236a7344c18a7411fa0164fbe9b3ce
darren.ng <darren.ng@dpodium.com>
2020-04-02 13:24:11 +0800

    Update script

---
+++ update.sh
@@ -0,0 +1,28 @@
+newsha=$(curl "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=Urbg1xsxwde8bdlQ&suffix=tar.gz.sha256")
+echo $i
+oldsha=$(cat "db/sha256")
+
+if [ "$newsha" == "$oldsha" ]; then
+    echo "No update."
+    exit
+fi
+
+echo "Update found."
+wget -O "db/db.tar.gz" "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=Urbg1xsxwde8bdlQ&suffix=tar.gz"
+tar --strip-components=1 -zxvf "db/db.tar.gz" -C "db/"
+rm -f "db/db.tar.gz"
+git commit -am "db update"
+echo "$newsha" > "db/sha256"
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "----------"
+echo "New update, please tag and push"
+echo "----------"
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
